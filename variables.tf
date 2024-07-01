ca639c133f23bb61632ee034fcd3dac2d1a9634b
nimisha-gj <nimisha@infraspec.dev>
2024-07-01 19:01:54 +0530

    feat: adds code to create aws organization

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_service_access_principals" {
+  description = "(Optional) List of AWS service principal names for which you want to enable integration with your organization."
+  type        = list(string)
+  default = []
+}
+
+variable "enabled_policy_types" {
+  description = "(Optional) List of Organizations policy types to enable in the Organization Root."
+  type        = list(string)
+  default = []
+}
+
+variable "feature_set" {
+  description = "(Optional) Specify \"ALL\" (default) or \"CONSOLIDATED_BILLING\"."
+  type        = string
+}
