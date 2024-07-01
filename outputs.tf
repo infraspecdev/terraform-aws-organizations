361f844821a1036b70e92cd45fcb8b90be3720c3
nimisha-gj <nimisha@infraspec.dev>
2024-07-02 01:01:06 +0530

    fix: fixes organization creation failure

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,39 @@
 output "aws_organizations_organization_accounts" {
   description = "List of organization accounts including the master account."
-  value       = aws_organizations_organization.this.accounts
+  value       = aws_organizations_organization.org.accounts
 }
 
 output "aws_organizations_organization_arn" {
   description = "ARN of the organization."
-  value       = aws_organizations_organization.this.arn
+  value       = aws_organizations_organization.org.arn
 }
 
 output "aws_organizations_organization_id" {
   description = "Identifier of the organization."
-  value       = aws_organizations_organization.this.id
+  value       = aws_organizations_organization.org.id
 }
 
 output "aws_organizations_organization_master_account_arn" {
   description = "ARN of the master account."
-  value       = aws_organizations_organization.this.master_account_arn
+  value       = aws_organizations_organization.org.master_account_arn
 }
 
 output "aws_organizations_organization_master_account_email" {
   description = "Email address of the master account."
-  value       = aws_organizations_organization.this.master_account_email
+  value       = aws_organizations_organization.org.master_account_email
 }
 
 output "aws_organizations_organization_master_account_id" {
   description = "Identifier of the master account."
-  value       = aws_organizations_organization.this.master_account_id
+  value       = aws_organizations_organization.org.master_account_id
 }
 
 output "aws_organizations_organization_none_master_accounts" {
   description = "List of organization accounts excluding the master account."
-  value       = aws_organizations_organization.this.non_master_accounts
+  value       = aws_organizations_organization.org.non_master_accounts
 }
 
 output "aws_organizations_organization_roots" {
   description = "List of organization roots."
-  value       = aws_organizations_organization.this.roots
+  value       = aws_organizations_organization.org.roots
 }
