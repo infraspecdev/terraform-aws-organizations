f72b1a4d4cd9ebbbb34e654ade5d8cdb545c4509
nimisha-gj <nimisha@infraspec.dev>
2024-07-02 01:01:41 +0530

    fix: fixes ouput failure of organization unit

--- modules/organizational_unit/outputs.tf
+++ modules/organizational_unit/outputs.tf
@@ -1,14 +1,14 @@
 output "aws_organizations_organizational_unit_accounts" {
-  description = "List of child accounts for this Organizational Unit."
-  value       = aws_organizations_organizational_unit.this.accounts
+  description = "List of child accounts for org_unit Organizational Unit."
+  value       = aws_organizations_organizational_unit.org_unit.accounts
 }
 
 output "aws_organizations_organizational_unit_arn" {
   description = "ARN of the organizational unit."
-  value       = aws_organizations_organizational_unit.this.arn
+  value       = aws_organizations_organizational_unit.org_unit.arn
 }
 
 output "aws_organizations_organizational_unit_id" {
   description = "Identifier of the organization unit."
-  value       = aws_organizations_organizational_unit.this.id
+  value       = aws_organizations_organizational_unit.org_unit.id
 }
