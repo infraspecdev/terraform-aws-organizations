381394bdf882c8d11bc985f6e2c2a96328afc2d4
nimisha-gj <nimisha@infraspec.dev>
2024-07-02 15:42:35 +0530

    refactor: renames variables in examples

--- examples/create-organization/main.tf
+++ examples/create-organization/main.tf
@@ -1,7 +1,7 @@
 module "service_integration_enabled_org" {
   source = "../../"
 
-  service_access_principals = local.aws_organizations_aws_service_access_principals
-  enabled_policy_types      = local.aws_organizations_enabled_policy_types
-  feature_set               = local.aws_organizations_feature_set
+  aws_service_access_principals = local.aws_organizations_aws_service_access_principals
+  aws_enabled_policy_types      = local.aws_organizations_enabled_policy_types
+  aws_feature_set               = local.aws_organizations_feature_set
 }
