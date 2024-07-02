373db07fb1c7a7a6dddf05cbfe2661b5598f71b7
nimisha-gj <nimisha@infraspec.dev>
2024-07-02 15:38:44 +0530

    renames variable in aws_prganizations_organization resource

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-variable "service_access_principals" {
+variable "aws_service_access_principals" {
   description = "(Optional) List of AWS service principal names for which you want to enable integration with your organization."
   type        = list(string)
   default     = []
 }
 
-variable "enabled_policy_types" {
+variable "aws_enabled_policy_types" {
   description = "(Optional) List of Organizations policy types to enable in the Organization Root."
   type        = list(string)
   default     = []
 }
 
-variable "feature_set" {
+variable "aws_feature_set" {
   description = "(Optional) Specify \"ALL\" (default) or \"CONSOLIDATED_BILLING\"."
   type        = string
 }
