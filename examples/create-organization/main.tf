340dbf10de880ca9dc806c69f2b3528c5e16c030
nimisha-gj <nimisha@infraspec.dev>
2024-07-02 12:47:46 +0530

    refactor: updates variable name in aws_organizations_organization to follow best practices

--- examples/create-organization/main.tf
+++ examples/create-organization/main.tf
@@ -1,7 +1,7 @@
 module "service_integration_enabled_org" {
   source = "../../"
 
-  aws_service_access_principals = local.aws_organizations_aws_service_access_principals
-  enabled_policy_types          = local.aws_organizations_enabled_policy_types
-  feature_set                   = local.aws_organizations_feature_set
+  service_access_principals = local.aws_organizations_aws_service_access_principals
+  enabled_policy_types      = local.aws_organizations_enabled_policy_types
+  feature_set               = local.aws_organizations_feature_set
 }
