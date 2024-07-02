dbf38d3d3a5955b6838ff6c33dc14f202c654792
nimisha-gj <nimisha@infraspec.dev>
2024-07-02 11:54:00 +0530

    adds example to create organization

---
+++ examples/create-organization/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  aws_region                                      = "ap-south-1"
+  aws_organizations_feature_set                   = "ALL"
+  aws_organizations_enabled_policy_types          = []
+  aws_organizations_aws_service_access_principals = []
+}
