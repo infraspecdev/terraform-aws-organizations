1427b4157adde6f94dc232aea43901cc8ec0827e
nimisha-gj <nimisha@infraspec.dev>
2024-07-05 13:32:23 +0530

    feat: resource creation defaults to root if not specified

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -1,7 +1,7 @@
 resource "aws_organizations_account" "member_account" {
   name                       = var.name
   email                      = var.email
-  parent_id                  = var.parent_id
+  parent_id                  = var.parent_id != "" ? var.parent_id : data.aws_organizations_organization.org.roots[0].id
   iam_user_access_to_billing = var.iam_user_access_to_billing
   tags                       = var.tags
   close_on_deletion          = var.close_on_deletion
--- modules/organizational_unit/main.tf
+++ modules/organizational_unit/main.tf
@@ -1,5 +1,5 @@
 resource "aws_organizations_organizational_unit" "org_unit" {
   name      = var.name
-  parent_id = var.parent_id
+  parent_id = var.parent_id != "" ? var.parent_id : data.aws_organizations_organization.org.roots[0].id
   tags      = var.tags
 }
