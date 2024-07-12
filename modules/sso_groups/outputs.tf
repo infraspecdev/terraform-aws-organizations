cd1a2b77a489f112b18e51081d7531eff4df82c9
ShivaniBanke <shivani@infraspec.dev>
2024-07-12 08:13:46 +0530

    chore: apply tflint

--- modules/sso_groups/outputs.tf
+++ modules/sso_groups/outputs.tf
@@ -1,4 +1,4 @@
-output "sso_group_IDs" {
+output "sso_group_ids" {
   description = "A map of SSO groups IDs created by this module"
   value       = { for k, v in aws_identitystore_group.this : k => v.group_id }
 
--- modules/sso_groups/variables.tf
+++ modules/sso_groups/variables.tf
@@ -1,4 +1,9 @@
 variable "sso_groups" {
+    description = <<EOF
+  A map of objects defining AWS SSO groups to be created. Each object contains:
+  - group_name: The name of the SSO group.
+  - group_description: A description for the SSO group.
+  EOF
   type = map(object(
     {
       group_name        = string
