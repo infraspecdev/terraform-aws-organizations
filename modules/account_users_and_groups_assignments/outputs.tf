b8a2891131799f7659b77180b3daf6ce872dc2cf
ShivaniBanke <shivani@infraspec.dev>
2024-07-16 11:49:04 +0530

    chore : add description to the output

--- modules/account_users_and_groups_assignments/outputs.tf
+++ modules/account_users_and_groups_assignments/outputs.tf
@@ -1,4 +1,4 @@
 output "assignments" {
   description = "The account assignment resources created for AWS SSO. Each resource includes details about the account, permission set, principal, and the status of the assignments."
-  value = aws_ssoadmin_account_assignment.this
+  value       = aws_ssoadmin_account_assignment.this
 }
