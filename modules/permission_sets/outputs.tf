d7667f7243fb1a1211bc5fdd768610ecbd7b5e07
ShivaniBanke <shivani@infraspec.dev>
2024-07-17 09:35:29 +0530

    chore: change output name

--- modules/permission_sets/outputs.tf
+++ modules/permission_sets/outputs.tf
@@ -1,4 +1,4 @@
-output "permission_sets" {
+output "permission_sets_arns" {
   description = "A map of the permission sets that were created. Each key is the name of the permission set, and the value contains the ARNs of the created permission sets."
   value       = { for k, v in aws_ssoadmin_permission_set.this : k => v.arn }
 }
