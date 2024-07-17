7a968d47a19c423dc7d1545ebe89af417e7b47f0
ShivaniBanke <shivani@infraspec.dev>
2024-07-17 09:24:36 +0530

    chore: refactor the output value

--- modules/permission_sets/outputs.tf
+++ modules/permission_sets/outputs.tf
@@ -1,4 +1,4 @@
 output "permission_sets" {
-  description = "A map of the permission sets that were created. Each key is the name of the permission set, and the value contains the details of the created permission set."
-  value       = aws_ssoadmin_permission_set.this
+  description = "A map of the permission sets that were created. Each key is the name of the permission set, and the value contains the ARNs of the created permission sets."
+  value       = { for k, v in aws_ssoadmin_permission_set.this : k => v.arn }
 }
