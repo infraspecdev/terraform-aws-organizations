66e01402a5ac959e0e48818db7f387ced5277632
nimisha-gj <nimisha@infraspec.dev>
2024-07-25 17:29:52 +0530

    feat: support policy attachment by default in sub module

--- examples/create-standard-organization-policies/main.tf
+++ examples/create-standard-organization-policies/main.tf
@@ -15,11 +15,5 @@ module "tag_policy" {
   description = local.tag_policy_description
   type        = local.tag_policy_type
   tags        = local.default_tags
-}
-
-
-module "attach_tag_policy" {
-  source    = "../../modules/policy_attachment"
-  policy_id = module.tag_policy.aws_organizations_policy_id
-  target_id = local.tag_policy_taget_id
+  target_id   = local.tag_policy_taget_id
 }
